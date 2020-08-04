524da759571a64bb5840b61af51915f6f6d3f849
Masaya Nakamura <mashabow@shiromoji.net>
2020-08-05 00:02:46 +0900

    feat: implement the modal view and submit event listener

--- functions/src/slack.tsx
+++ functions/src/slack.tsx
@@ -1,5 +1,11 @@
 /** @jsx JSXSlack.h **/
-import { JSXSlack, Modal, Section } from "@speee-js/jsx-slack";
+import {
+  JSXSlack,
+  Modal,
+  Input,
+  Textarea,
+  UsersSelect,
+} from "@speee-js/jsx-slack";
 import * as functions from "firebase-functions";
 import { App, ExpressReceiver } from "@slack/bolt";
 
@@ -18,12 +24,29 @@ const app = new App({
 
 export const slackApp = expressReceiver.app;
 
+const ID = {
+  SUBMIT_CALLBACK: "submit_callback",
+  MEMBERS: "members",
+  MESSAGE: "message",
+  CHANNEL: "channel",
+} as const;
+
 app.command("/rota", async ({ ack, body, context }) => {
   await ack();
 
   const modal = JSXSlack(
-    <Modal title="Rota">
-      <Section>Hello, world!</Section>
+    <Modal callbackId={ID.SUBMIT_CALLBACK} title="Rota" close="キャンセル">
+      <UsersSelect
+        id={ID.MEMBERS}
+        name={ID.MEMBERS}
+        required
+        multiple
+        label="メンバー"
+      />
+      <Textarea id={ID.MESSAGE} name={ID.MESSAGE} required label="メッセージ" />
+      {/* TODO: 日時指定 */}
+      <Input type="hidden" name={ID.CHANNEL} value={body.channel_id} />
+      <Input type="submit" value="登録" />
     </Modal>
   );
 
@@ -37,4 +60,17 @@ app.command("/rota", async ({ ack, body, context }) => {
   } catch (error) {
     functions.logger.error("error", { error });
   }
+});
+
+app.view(ID.SUBMIT_CALLBACK, async ({ ack, body, view, context }) => {
+  await ack();
+  functions.logger.info("body", { body });
+
+  const values = {
+    members: view.state.values[ID.MEMBERS][ID.MEMBERS].selected_users,
+    message: view.state.values[ID.MESSAGE][ID.MESSAGE].value,
+    channel: JSON.parse(view.private_metadata)[ID.CHANNEL],
+  };
+  const user = body.user.id;
+  functions.logger.info("values, user", { values, user });
 });
