ba5073c2e9ca7778a7beed98b77dcded65427f86
Masaya Nakamura <mashabow@shiromoji.net>
2021-09-25 23:07:30 +0900

    refactor: apply prettier

--- functions/src/cron.ts
+++ functions/src/cron.ts
@@ -3,17 +3,21 @@ import { Rotation } from "./model/rotation";
 import { Schedule } from "./model/schedule";
 import { RotationStore } from "./store";
 
-export const cronHandler = (
-  rotationStore: RotationStore,
-  postRotation: (rotation: Rotation) => Promise<void>
-) => async (context: functions.EventContext): Promise<void> => {
-  const schedule = Schedule.dateToNearestSchedule(new Date(context.timestamp));
-  const rotations = await rotationStore.getBySchedule(schedule);
-  functions.logger.log("rotations", { rotations });
+export const cronHandler =
+  (
+    rotationStore: RotationStore,
+    postRotation: (rotation: Rotation) => Promise<void>
+  ) =>
+  async (context: functions.EventContext): Promise<void> => {
+    const schedule = Schedule.dateToNearestSchedule(
+      new Date(context.timestamp)
+    );
+    const rotations = await rotationStore.getBySchedule(schedule);
+    functions.logger.log("rotations", { rotations });
 
-  for (const rotation of rotations) {
-    const newRotation = rotation.rotate();
-    await rotationStore.set(newRotation);
-    await postRotation(newRotation);
-  }
-};
+    for (const rotation of rotations) {
+      const newRotation = rotation.rotate();
+      await rotationStore.set(newRotation);
+      await postRotation(newRotation);
+    }
+  };
