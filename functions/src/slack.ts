51dda668433cc68f28a84e3e82f5fcded33402bd
Masaya Nakamura <mashabow@shiromoji.net>
2021-10-02 23:56:34 +0900

    feat: initialize ExpressReciever with options for OAuth

--- functions/src/slack.ts
+++ functions/src/slack.ts
@@ -26,14 +26,52 @@ export const createSlackApp = (
 } => {
   const expressReceiver = new ExpressReceiver({
     signingSecret: config.slack.signing_secret,
+    clientId: config.slack.client_id,
+    clientSecret: config.slack.client_secret,
+    // OAuth の state を生成するときの salt のようなもの
+    // Firebase だと stateStore を自前で用意する必要ある？
+    stateSecret: "my-state-secret",
+    scopes: ["chat:write", "chat:write.public", "commands", "users:read"],
+    installationStore: {
+      storeInstallation: async (installation) => {
+        if (installation.isEnterpriseInstall && installation.enterprise) {
+          // OrG 全体へのインストールに対応する場合
+          return await database.set(installation.enterprise.id, installation);
+        }
+        if (installation.team) {
+          // 単独のワークスペースへのインストールの場合
+          return await database.set(installation.team.id, installation);
+        }
+        throw new Error("Failed saving installation data to installationStore");
+      },
+      fetchInstallation: async (installQuery) => {
+        if (installQuery.isEnterpriseInstall && installQuery.enterpriseId) {
+          // OrG 全体へのインストール情報の参照
+          return await database.get(installQuery.enterpriseId);
+        }
+        if (installQuery.teamId) {
+          // 単独のワークスペースへのインストール情報の参照
+          return await database.get(installQuery.teamId);
+        }
+        throw new Error("Failed fetching installation");
+      },
+      deleteInstallation: async (installQuery) => {
+        if (installQuery.isEnterpriseInstall && installQuery.enterpriseId) {
+          // OrG 全体へのインストール情報の削除
+          return await database.delete(installQuery.enterpriseId);
+        }
+        if (installQuery.teamId) {
+          // 単独のワークスペースへのインストール情報の削除
+          return await database.delete(installQuery.teamId);
+        }
+        throw new Error("Failed to delete installation");
+      },
+    },
     endpoints: "/events",
     processBeforeResponse: true,
   });
 
-  const app = new App({
-    receiver: expressReceiver,
-    token: config.slack.bot_token,
-  });
+  const app = new App({ receiver: expressReceiver });
 
   app.use(async ({ context, next }) => {
     context.rota = { rotationStore };
